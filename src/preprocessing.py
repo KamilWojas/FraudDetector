35d550271e9777dcd38bca29dae3d5be7d6f8eb7
KamilWojas <wojaskamil@yahoo.pl>
2025-02-24 22:22:36 +0100

    preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,15 +1,24 @@
 import pandas as pd
+import os
 
-DATA_PATH = "../data/raw/creditcard.csv"  # Sprawdź tę ścieżkę
+# Pobierz pełną ścieżkę do pliku
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_PATH = os.path.join(BASE_DIR, "../data/raw/creditcard.csv")
+
 
 def load_data():
     """Wczytuje dane z pliku CSV."""
+    if not os.path.exists(DATA_PATH):
+        raise FileNotFoundError(f"Plik nie istnieje: {DATA_PATH}")
+
     df = pd.read_csv(DATA_PATH)
     print(f"Załadowano {len(df)} rekordów.")
     return df
 
+
 if __name__ == "__main__":
     df = load_data()
     print(df.head())
+
 
 
